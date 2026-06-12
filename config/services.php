25f58ddd3d35e940fb84ed5df393f67da9ff60ea
Gavin Kimpson <gkimpson@gmail.com>
2026-06-12 07:20:46 +0100

    feat: add API-Football service config

--- config/services.php
+++ config/services.php
@@ -35,4 +35,11 @@ return [
         ],
     ],
 
+    'api_football' => [
+        'key' => env('API_FOOTBALL_KEY'),
+        'base_url' => env('API_FOOTBALL_BASE_URL', 'https://v3.football.api-sports.io'),
+        'league' => (int) env('API_FOOTBALL_LEAGUE', 1),
+        'season' => (int) env('API_FOOTBALL_SEASON', 2026),
+    ],
+
 ];
