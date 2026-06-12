aac09e724ef9ce0e9d9d5e557aace9d8f89f0bac
Gavin Kimpson <gkimpson@gmail.com>
2026-06-12 07:17:57 +0100

    feat: add Competition model as reference table for provider league/season

---
+++ app/Models/Competition.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int|null $provider_league_id
+ * @property int|null $season
+ */
+#[Fillable(['name', 'slug', 'provider_league_id', 'season'])]
+class Competition extends Model
+{
+    protected function casts(): array
+    {
+        return [
+            'provider_league_id' => 'integer',
+            'season' => 'integer',
+        ];
+    }
+}
